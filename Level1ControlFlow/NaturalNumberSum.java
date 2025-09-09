9cb3d20669ff40dbb56a8e8709bf8e6e2a614489
Shikha-7038 <shikhagahlawat2@gmail.com>
2025-09-09 12:23:24 +0530

    new

---
+++ Level1ControlFlow/NaturalNumberSum.java
@@ -0,0 +1,21 @@
+package Level1ControlFlow;
+
+import java.util.Scanner;
+
+public class NaturalNumberSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n >= 1) {  // Natural numbers are 1,2,3,...
+            int sum = n * (n + 1) / 2;
+            System.out.println("The sum of " + n + " natural numbers is " + sum);
+        } else {
+            System.out.println("The number " + n + " is not a natural number");
+        }
+        sc.close();
+    }
+}
+
